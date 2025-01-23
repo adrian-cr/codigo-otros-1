b89ec5e4c9fcde35ed9b3900a482733f13d05f02
Adrian Camacho <acr77frb@gmail.com>
2025-01-23 11:02:05 -0600

    Se gregaron correcciones al código original

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,24 @@
-const baseEndpoint = 'https://api.github.com';
+const baseEndpoint = "https://api.github.com";
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+/*Se modificaron los nombres de las constantes $n (nameElement), $b (blogElement) y $l (locationElement) para mayor legibilidad.*/
+const nameElement = document.querySelector(".name"); //Punto faltante en '.name'.
+const blogElement = document.querySelector(".blog"); //Marcador equivocado en '#blog'; debe ser '.blog'.
+const locationElement = document.querySelector(".location");
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
+async function displayUser(username) { //Se agregó la palabra reservada 'async' para permitir el funcionamiento de la función asíncrona.
+  nameElement.textContent = "cargando...";
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  const data = await response.json(); //Expresión faltante; no se convirtió la respuesta de la API en un objeto JSON.
+  /* Se modificaron los operadores "" en las siguientes tres líneas por `` para permitir la interpolación de strings.*/
+  nameElement.textContent = `${data.name}`;
+  blogElement.textContent = `${data.blog}`;
+  locationElement.textContent = `${data.location}`;
 }
 
 function handleError(err) {
-  console.log('OH NO!');
+  console.log("OH NO!");
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  nameElement.textContent = `Algo salió mal: ${err}`; //Se corrigió el nombre de la constante $n (ahora nameElement); se había llamado con el nombre 'n', sin el prefijo '$'.
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser("stolinski").catch(handleError);
